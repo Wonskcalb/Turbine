e41b5aa32b644d28a3920b967d71ba014094102c
Valentin Famelart <famelartv@gmail.com>
2025-06-01 16:52:01 +0200

    feat(recipe): add a print method

--- src/library/models.py
+++ src/library/models.py
@@ -1,3 +1,5 @@
+from textwrap import dedent, indent
+
 from django.db import models
 
 from pantry.models import Ingredient, Utensil
@@ -10,8 +12,8 @@ class Source(models.Model):
 
     name = models.CharField(max_length=256)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class CuisineType(models.Model):
@@ -21,8 +23,8 @@ class CuisineType(models.Model):
 
     name = models.CharField(max_length=256)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 class Recipe(models.Model):
@@ -39,5 +41,34 @@ class Recipe(models.Model):
     utensils = models.ManyToManyField(to=Utensil)
     ingredients = models.ManyToManyField(to=Ingredient)
 
-    def __str__(self):
-        return self.name
+    def __str__(self) -> str:
+        return str(self.name)
+
+    
+    def show(self):
+        """
+        Show what the recipe is about.
+        """
+
+        kinds = ', '.join(str(k) for k in self.kind.all())
+        ingredients = '\n'.join(
+            str(ingredient) for ingredient in self.ingredients.all()
+        )
+        utensils = '\n'.join(str(utentil) for utentil in self.utensils.all())
+
+        tmpl = dedent(
+            f"""
+            {self.name}, a %(kinds)s recipe
+            ---
+            What you'll need to by:
+            %(ingredients)s
+            What you'll need in the kitchen:
+            %(utensils)s
+            """
+        ) % {
+            "kinds": kinds,
+            "ingredients": indent(ingredients, "  - "),
+            "utensils": indent(utensils, "  - "),
+        }
+
+        return tmpl
